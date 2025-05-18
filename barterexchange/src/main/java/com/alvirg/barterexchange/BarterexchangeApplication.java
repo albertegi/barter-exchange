e65451c7d9c30bb567fd4d47979b1f091ffef735
albertegi <albertegi90@gmail.com>
2025-05-18 01:36:13 +0100

    Initial commit

---
+++ barterexchange/src/main/java/com/alvirg/barterexchange/BarterexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.alvirg.barterexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarterexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarterexchangeApplication.class, args);
+	}
+
+}
---
+++ barterexchange/src/test/java/com/alvirg/barterexchange/BarterexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvirg.barterexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarterexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
